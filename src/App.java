c4d2d93da1d062af768f77c4a70cce5df1932c6c
Arlyjr <arly.lcj@gmail.com>
2024-09-09 23:32:35 -0300

    aprendizados e projetos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World! novo projeto e aprendizado em Java");
+        System.out.println("Hello World! reinicio de novos aprendizados e projeto em liguagem Java");
     }
 }
