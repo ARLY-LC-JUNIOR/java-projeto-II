42c32f9b50687e71b575b522bc5b6cbb0e43c7ca
Arlyjr <arly.lcj@gmail.com>
2024-09-09 23:01:18 -0300

    atualizado II

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World! Reiniciando novo projeto e aprendizado em java");
+        System.out.println("Hello World! Reiniciando novo projeto e aprendizado em Java");
     }
 }
